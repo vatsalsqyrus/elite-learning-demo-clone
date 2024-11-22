1168e131d4e3af6e469d55f8d111667d2c0c776d
vatsalsqyrus <vatsals@quinnox.com>
2024-11-22 15:10:54 +0530

    vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,12 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';  // Change from adapter-auto to adapter-vercel
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			runtime: 'edge'  // Specify edge runtime to avoid Node.js version issues
+		})
 	},
 	preprocess: vitePreprocess()
 };
